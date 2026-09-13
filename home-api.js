436b21d1b463bca0b101c1e9c43bc856190457ea
Free <rustamdzuraev08@gmail.com>
2026-09-13 20:07:27 +0500

    Fix home loader and remove Shikimori dependency from home

---
+++ home-api.js
@@ -0,0 +1,17 @@
+async function loadHomeFromApi(){
+  const sections=[
+    ['recommendations',{limit:12,order:'popularity',page:2}],
+    ['popular',{limit:12,order:'popularity',page:1}],
+    ['newAnime',{limit:12,order:'aired_on',page:1}],
+    ['rated',{limit:12,order:'ranked',page:1}]
+  ];
+  try{
+    const results=await Promise.all(sections.map(async([id,params])=>[id,await getAnimes(params)]));
+    results.forEach(([id,list])=>renderApiRow(id,list));
+  }catch(error){
+    console.error('AnimeSpace home API:',error);
+  }
+}
+
+if(typeof window.loadTopFromApi!=='function') window.loadTopFromApi=async()=>{};
+if(typeof window.loadScheduleFromApi!=='function') window.loadScheduleFromApi=async()=>{};
